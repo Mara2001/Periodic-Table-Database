a168f55a7dd92ddb451ae6ca1dbfa600b10ddcdc
Marek Nedvěd <nedvem@gmail.com>
2024-12-22 22:37:29 +0000

    feat: responces element data

--- element.sh
+++ element.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+fi
+
+INPUT=$1
+#echo $INPUT
+
+# test zda je vstup cislo
+if [[ $INPUT =~ [0-9]+ ]]
+then
+  #echo "Number"
+  RESULT="$($PSQL "SELECT * FROM elements WHERE atomic_number=$INPUT;")"  
+# test zda je vztup znacka
+elif [[ ${#INPUT} -le 2 ]]
+then
+  #echo "Symbol"
+  RESULT="$($PSQL "SELECT * FROM elements WHERE symbol='$INPUT';")"
+# vstup musi byt nazev
+else
+  #echo "Nazev"
+  RESULT="$($PSQL "SELECT * FROM elements WHERE name='$INPUT';")"
+fi
+echo $RESULT
